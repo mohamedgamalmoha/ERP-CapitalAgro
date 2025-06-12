72c5237ded33637f6355fb3eecda216cfd67844d
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-06-13 02:44:20 +0300

    Add list filter to admin class

--- accounts/admin.py
+++ accounts/admin.py
@@ -8,6 +8,7 @@ from accounts.models import User
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
+    list_filter = ("is_staff", "is_superuser", "is_active", "role")
     fieldsets = (
         (None, {"fields": ("username", "password")}),
         (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
