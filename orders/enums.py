947e0c80d89bf33d29fc626de7b948919c65139e
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-06-15 01:53:29 +0300

    Add enum related constants

--- orders/enums.py
+++ orders/enums.py
@@ -9,3 +9,42 @@ class OrderStatus(models.IntegerChoices):
     READY = 3, _('Ready')
     DELIVERED = 4, _('Delivered')
     CANCELLED = 5, _("Cancelled")
+
+
+ORDER_STATUS_SEQUENCE = {
+    OrderStatus.PENDING: [OrderStatus.CONFIRMED, OrderStatus.CANCELLED],
+    OrderStatus.CONFIRMED: [OrderStatus.PREPARING, OrderStatus.READY, OrderStatus.CANCELLED],
+    OrderStatus.PREPARING: [OrderStatus.READY],
+    OrderStatus.READY: [OrderStatus.DELIVERED],
+    OrderStatus.DELIVERED: [],
+    OrderStatus.CANCELLED: [],
+}
+
+ORDER_STATUS_AVAILABILITY_CHECK = [
+    OrderStatus.PENDING,
+    OrderStatus.CONFIRMED,
+    OrderStatus.PREPARING
+]
+
+ORDER_STATUS_APPLY_CONSUMPTION = {
+    OrderStatus.CONFIRMED: [
+        OrderStatus.PREPARING,
+        OrderStatus.READY
+    ]
+}
+
+ORDER_STATUS_APPLY_RESTORATION = {
+    OrderStatus.PENDING: [
+        OrderStatus.CANCELLED
+    ],
+    OrderStatus.CONFIRMED: [
+        OrderStatus.CANCELLED
+    ],
+}
+
+ORDER_STATUS_DENY_ITEMS_MODIFICATION = [
+    OrderStatus.PREPARING,
+    OrderStatus.READY,
+    OrderStatus.DELIVERED,
+    OrderStatus.CANCELLED
+]
