f1f6526320142841b0d590160fc3b4e84d355b0b
Youssefshawky969 <168371773+Youssefshawky969@users.noreply.github.com>
2025-06-16 22:00:06 +0300

    Update wsgi.py
    Modify wsgi.py to Load .env Automatically

--- core/wsgi.py
+++ core/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
+from dotenv import load_dotenv
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+load_dotenv()
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 application = get_wsgi_application()
